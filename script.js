309580c46a0e5841d50f6b73210faf24919d6d6e
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2026-09-21 19:32:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// let num1 = 8
-// let num2 = 2
-let num1;
-let num2;
+let num1 = 8
+let num2 = 2
 let sumTxt = "Sum: "
 document.getElementById("no1").textContent = num1
 document.getElementById("no2").textContent = num2
